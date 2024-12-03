f75bb2e9bf04c0483e5cc1b2b64221b7b28a1f9b
Eduardo <EduardoAC@users.noreply.github.com>
2024-12-03 12:02:24 +0000

    Test release propagation

--- packages/ui-metrics/src/components/usage.ts
+++ packages/ui-metrics/src/components/usage.ts
@@ -9,11 +9,14 @@ export async function getComponentsUsage(
   projectDir: string = "."
 ): Promise<TotalNamedImports> {
   try {
-    console.log(`Scanning your project for referenced imports by ${packageName} within ${projectDir} directory`);
+    console.log(
+      `Scanning your project for referenced imports by ${packageName} within ${projectDir} directory`
+    );
     const imports = await searchFilesForImport(projectDir, packageName);
     const componentsByImport = countNamedImports(imports);
     return componentsByImport;
   } catch (error: unknown) {
+    console.error("Unavailable to execute component usage successfully");
     throw new Error("Unavailable to execute component usage successfully", {
       cause: error,
     });
